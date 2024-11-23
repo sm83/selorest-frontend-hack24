45b1f8e3d43e4b8fba31e78622f10c918ddad357
Nikita <corasks@yandex.ru>
2024-11-23 07:57:17 +0300

    [+] component bils

--- src/app/initial/page.tsx
+++ src/app/initial/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import "./page.scss";
-import Image from "next/image";
 import React from "react";
 import { useRouter } from "next/navigation";
 
---
+++ src/app/test/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Bils from "@/components/bils/Bils";
+import React from "react";
+
+const Test = () => {
+  return (
+    <div style={{ display: "flex", alignItems: "flex-end", height: "100%" }}>
+      <Bils />
+    </div>
+  );
+};
+
+export default Test;
