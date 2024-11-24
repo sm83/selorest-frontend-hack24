31c35f0c5bd3175c8a24603e0ed1dcf8baf8b9aa
p-shcherbatykh <p.shcherbatykh@redlight.group>
2024-11-24 04:50:22 +0300

    categories

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
-import profile from "./slices/profileSlice"
+import profile from "./slices/profileSlice";
+import categories from "./slices/categoriesSlice";
 
 export default combineReducers({
-    profile
+	profile,
+	categories,
 });
